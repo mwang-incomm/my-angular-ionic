73d10afb4600cce61655681511188243522aedc3
Marty Wang <mwang@incomm.com>
2024-10-15 13:08:20 -0600

    base project

---
+++ src/main.ts
@@ -0,0 +1,85 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+import { Capacitor } from '@capacitor/core';
+import {
+  AgentConfiguration,
+  NewRelicCapacitorPlugin,
+  NREnums,
+} from '@newrelic/newrelic-capacitor-plugin';
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
+
+var appToken;
+
+if (Capacitor.getPlatform() === 'ios') {
+  appToken = '<IOS-APP-TOKEN>';
+} else {
+  appToken = '<ANDROID-APP-TOKEN>';
+}
+
+let agentConfig: AgentConfiguration = {
+  //Android Specific
+  // Optional:Enable or disable collection of event data.
+  analyticsEventEnabled: true,
+
+  // Optional:Enable or disable crash reporting.
+  crashReportingEnabled: true,
+
+  // Optional:Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+  interactionTracingEnabled: true,
+
+  // Optional:Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+  networkRequestEnabled: true,
+
+  // Optional:Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+  networkErrorRequestEnabled: true,
+
+  // Optional:Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
+  httpResponseBodyCaptureEnabled: true,
+
+  // Optional:Enable or disable agent logging.
+  loggingEnabled: true,
+
+  // Optional:Specifies the log level. Omit this field for the default log level.
+  // Options include: ERROR (least verbose), WARNING, INFO, VERBOSE, AUDIT (most verbose).
+  logLevel: NREnums.LogLevel.INFO,
+
+  // iOS Specific
+  // Optional:Enable/Disable automatic instrumentation of WebViews
+  webViewInstrumentation: true,
+
+  // Optional:Set a specific collector address for sending data. Omit this field for default address.
+  // collectorAddress: "",
+
+  // Optional:Set a specific crash collector address for sending crashes. Omit this field for default address.
+  // crashCollectorAddress: "",
+
+  // Optional:Enable or disable sending JS console logs to New Relic.
+  sendConsoleEvents: true,
+
+  // Optional: Enable or disable reporting data using different endpoints for US government clients.
+  fedRampEnabled: false,
+
+  // Optional: Enable or disable offline data storage when no internet connection is available.
+  offlineStorageEnabled: true,
+
+  // iOS Specific
+  // Optional: Enable or disable Background Reporting.
+  backgroundReportingEnabled: true,
+
+  // iOS Specific
+  // Optional: Enable or disable to use our new, more stable, event system for iOS agent.
+  newEventSystemEnabled: true,
+
+  // Optional: Enable or disable distributed tracing.
+  distributedTracingEnabled: true,
+};
+
+NewRelicCapacitorPlugin.start({
+  appKey: appToken,
+  agentConfiguration: agentConfig,
+});
